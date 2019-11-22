23755f0ca2697f348b873cfdbd1ef7464aa7547b
PaulsBecks <paul.beck.berlin@gmail.com>
2019-11-23 00:05:10 +0100

    added lane to test

--- fasstrack-backend.py
+++ fasstrack-backend.py
@@ -8,21 +8,75 @@ timetable = {
   }
 }
 
+containerLines = {
+  "laneId1": {
+    bottom: {0:"fdsa", 1:"fdas"}
+    top: {0: "fda"}
+  },
+  "laneId2": {
+
+  }
+}
+
+cranes = {
+  "craneId": {
+    lane: "laneId"
+  },
+  "crandId": {
+    # lane less crane means he can get used everywhere
+  }
+}
+
 app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
     return "Hello, World!"
 
+"""
+  This method returns the timetable
+"""
 @app.route("/timetable")
 def getTimetable():
   return json.dumps(timetable)
 
+"""
+  POST: This method handles the submitted JSON data by addeding the truck update in the table or updating the existing data.
+  GET: Returns the current entry of a truck in the database
+"""
 @app.route("/timetable/<truckId>", methods=['POST', 'GET'])
 def updateTruck(truckId):
   if request.method == 'POST':
     timetable[truckId] = request.json
-    return request.json
+    # TODO: return the placeID where the truck should collect the container
+    timetable[truckId].lane = "dafe121"
+    return timetable[truckId]
   elif request.method == 'GET': 
     return json.dumps(timetable[truckId])
   return None
+
+"""
+  GET: get the next task for a crane
+"""
+@app.route("crane/<craneId>/nextTask", method=['GET'])
+def craneNextTask(craneId):
+  # TODO: get next step to optimize the stack
+  # is next container not available? -> make it accessable
+  # how much time till next service?
+  # if enough time to optimize:
+  #     if space to optimize: get a container
+  #     else: chill mate 
+  # else:
+  #     prepare delivery > delivery
+
+
+""""
+  POST: register an existing crane to handle tasks
+"""
+@app.route("crane/<craneId>/register", method=['POST'])
+def craneRegister(craneId):
+  # TODO: add crane into database
+  # if lanes not operated: put him into lane
+  # else: let him help without lane
+
+
